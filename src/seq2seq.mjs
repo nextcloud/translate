6d4b62341b49638396ec1b12ec2352bc31a5daaa
Marcel Klehr <mklehr@gmx.net>
2023-01-26 15:20:13 +0100

    Implement min_length and repetition_penalty

--- src/seq2seq.mjs
+++ src/seq2seq.mjs
@@ -15,8 +15,9 @@ const T5_pad_token_id = 0
 const T5_eos_token_id = 1
 const TEMPERATURE = 0.2
 const TOP_K = 50
+const VOC_SIZE = 250112
 
-async function main(inputText, strategy, max_length) {
+async function main(inputText, strategy, max_length, min_length, repetition_penalty = 1) {
 	if (inputText === '-') {
 		inputText = await getStdin()
 	}
@@ -27,6 +28,8 @@ async function main(inputText, strategy, max_length) {
 	let inputData = preprocessor.encodeIds(cleanText(inputText)).filter(el => el <= 250111 && el>=-250112); // TODO: Look into proper tokenization algo
 	let decoderInputData = Int32Array.from([T5_pad_token_id])
 	let generation = ''
+	const filterOutEOS = tf.tensor(Array(VOC_SIZE).fill(1).map((_, i) => T5_eos_token_id === i? -Infinity : 1))
+
 	for (let i = 0; i<max_length; i++) {
 		const inputTensor = new ort.Tensor('int64', BigInt64Array.from(Array.from(inputData).map(num => BigInt(num))), [1, inputData.length]);
 		const attentionMask = new ort.Tensor('int64', BigInt64Array.from(new Array(inputData.length).fill(1n)), [1, inputData.length]);
@@ -42,7 +45,22 @@ async function main(inputText, strategy, max_length) {
 		if (strategy === 'sampling') {
 			result = result.div(TEMPERATURE)
 		}
-		const probsSqueezed = result.squeeze([0]).unstack().pop()
+
+		let probsSqueezed = result.squeeze([0]).unstack().pop()
+
+		if (min_length && min_length > 0 && min_length > i) {
+			probsSqueezed = tf.where(tf.isInf(filterOutEOS), filterOutEOS, probsSqueezed)
+		}
+
+
+		if (repetition_penalty > 1) {
+			const decoderInput = tf.tensor(decoderInputData)
+			const scores = tf.gather(probsSqueezed, decoderInput)
+			const penalty = tf.where(tf.greater(scores, 0), tf.onesLike(scores).div(repetition_penalty), tf.onesLike(scores).mul(repetition_penalty))
+			const penalizedScores = scores.mul(penalty)
+			const scattered = tf.scatterND(decoderInput, penalizedScores, probsSqueezed.shape).squeeze()
+			probsSqueezed = tf.where(tf.notEqual(scattered, 0), scattered, probsSqueezed)
+		}
 
 		let id
 
@@ -59,11 +77,13 @@ async function main(inputText, strategy, max_length) {
 		const token = await id.data()
 		generation = preprocessor.decodeIds([...decoderInputData, ...token])
 		decoderInputData = Int32Array.from([...decoderInputData, ...token])
+
 		if (token[0] === T5_eos_token_id) {
 			break
 		}
+		console.log(generation)
 	}
 	return generation
 }
 
-main(process.argv[4], process.argv[2], parseInt(process.argv[3]));
+console.log(await main(process.argv[4], process.argv[2], parseInt(process.argv[3]), 100, 1.5));
