e3f7babdd3704e3f7a53c394e22ab85caba37ca9
Marcel Klehr <mklehr@gmx.net>
2023-02-10 15:12:25 +0100

    Refactor seq2seq.mjs to work with any opus model

--- src/seq2seq.mjs
+++ src/seq2seq.mjs
@@ -3,21 +3,11 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import tf from '@tensorflow/tfjs'
 import getStdin from "get-stdin";
-import vocab from '../models/vocab.json' assert { type: "json" };
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 global.__dirname = __dirname
-
-const T5_pad_token_id = 58100
-const T5_eos_token_id = 0
-const VOC_SIZE = 58101
-const TEMPERATURE = 0.5
-const REPETITION_PENALTY = 1
-const TOP_K = VOC_SIZE
-const TOP_P = 0.95
-
 
 async function main(model, inputText) {
 	if (inputText === '-') {
@@ -29,20 +19,25 @@ async function main(model, inputText) {
 		executionProviders: ['cuda', 'cpu']
 	})
 
-	const decoderSession = await ort.InferenceSession.create(__dirname+'/../models'+model+'/decoder_model.onnx', {
+	const decoderSession = await ort.InferenceSession.create(__dirname+'/../models/'+model+'/decoder_model.onnx', {
 		executionMode: 'sequential',
 		executionProviders: ['cuda', 'cpu']
 	})
 
+	const vocab = (await import('../models/'+model+'/vocab.json', { assert: { type: "json" } })).default
+	const reverseVocab = Object.fromEntries(Object.entries(vocab).map(([token, id]) => [id, token]))
+
+	const config = (await import('../models/'+model+'/config.json', { assert: { type: "json" } })).default
+	const pad_token_id = config.pad_token_id
+	const eos_token_id = config.eos_token_id
+
 	const generatedSentences = []
 
 	for (const sentence of inputText.split('.')) {
-		let inputData = tokenize(sentence).map(token => vocab[token])
-		const reverseVocab = Object.fromEntries(Object.entries(vocab).map(([token, id]) => [id, token]))
+		let inputData = tokenize(sentence, vocab).map(token => vocab[token])
 
-		let decoderInputData = Int32Array.from([T5_pad_token_id])
+		let decoderInputData = Int32Array.from([pad_token_id])
 		let generation = ''
-		const filterOutEOS = tf.tensor(Array(VOC_SIZE).fill(1).map((_, i) => T5_eos_token_id === i ? -Infinity : 1))
 		const inputTensor = new ort.Tensor('int64', BigInt64Array.from([...Array.from(inputData).map(num => BigInt(num)), 0n]), [1, inputData.length + 1]);
 		const attentionMask = new ort.Tensor('int64', BigInt64Array.from(new Array(inputData.length + 1).fill(1n)), [1, inputData.length + 1]);
 
@@ -51,7 +46,7 @@ async function main(model, inputText) {
 			attention_mask: attentionMask
 		})
 
-		for (let i = 0; i < max_length; i++) {
+		while (true) {
 			const decoderInputTensor = new ort.Tensor('int64', BigInt64Array.from(Array.from(decoderInputData).map(num => BigInt(num))), [1, decoderInputData.length]);
 
 			// feed inputs and run
@@ -63,14 +58,8 @@ async function main(model, inputText) {
 
 			const id = tf.tidy(() => {
 				let result = tf.tensor(results.logits.data, results.logits.dims)
-				if (strategy === 'sampling') {
-					result = result.div(TEMPERATURE)
-				}
-
 				let probsSqueezed = result.squeeze([0]).unstack().pop()
-
 				let id = tf.argMax(probsSqueezed)
-
 				return id
 			})
 
@@ -79,12 +68,16 @@ async function main(model, inputText) {
 
 			decoderInputData = Int32Array.from([...decoderInputData, ...token])
 
-			if (token[0] === T5_eos_token_id) {
+			if (token[0] === eos_token_id) {
 				break
 			}
 		}
 
-		generation = [...decoderInputData.slice(1, -1)].map(tokenId => reverseVocab[tokenId].replace('\u2581', ' ')).join('')
+		generation = [...decoderInputData.slice(1, -1)]
+			.map(tokenId => reverseVocab[tokenId])
+			.filter(Boolean)
+			.map(word => word.replace('\u2581', ' '))
+			.join('')
 		generatedSentences.push(generation.substring(1))
 	}
 
@@ -97,7 +90,7 @@ console.log(await main(
 ))
 
 
-function tokenize(text) {
+function tokenize(text, vocab) {
 	const words = text.replace(/\s/, ' ').replace('\n', ' ').split(' ').filter(word => word !== '')
 	const tokens = []
 	for (let i=0; i<words.length; i++) {
